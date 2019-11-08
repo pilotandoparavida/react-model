cfe384c439b24a4cf5982b46b50401e69f399aea
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-08 16:13:10 -0300

    Removida a necessidade de informar adm

--- src/Turma.js
+++ src/Turma.js
@@ -40,7 +40,7 @@ const TurmaSchema = new global.mongoose.Schema ({
     adm: {
         type: global.mongoose.Schema.Types.ObjectId,
         ref: 'Administrador',
-        required: true,
+        required: false,
         default: null
     }
 }, {
